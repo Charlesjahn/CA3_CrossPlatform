da5c6cc90c0a5744199bfccc07edc994d920a260
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-05-17 19:27:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
-window.start_loader = function() {
+window.start_loader = function () {
     const loader = document.getElementById('loader-holder')
     loader.style.display = 'flex';
 }
-window.end_loader = function() {
+window.end_loader = function () {
     const loader = document.getElementById('loader-holder')
     loader.style.display = 'none';
 }
 
-window.onload = function() {
+window.onload = function () {
     setTimeout(() => {
         end_loader()
     }, 500)
 
     const loanForm = document.getElementById('calculate-loan-form')
-    loanForm.addEventListener('submit', function(e) {
+    loanForm.addEventListener('submit', function (e) {
         e.preventDefault()
         start_loader();
-        const principalAmount = document.getElementById('number_Units').value;
+        const total_Of_Units = document.getElementById('number_Units').value;
         const interest = document.getElementById('vat').value;
-        const PayableYears = document.getElementById('days_for_bill').value;
+        const daysOfBill = document.getElementById('days_for_bill').value;
+        const electriRatefinal = document.getElementById('electriRate').value;
         var monthly = 0,
             pmt = 0,
             total = 0,
             totalInterest = 0,
             monthlyInterest = 0;
-        monthlyInterest = (parseFloat(principalAmount) * (parseFloat(interest) / 100)) / 12;
-        pmt = parseFloat(monthlyInterest) / (1 - Math.pow(1 + ((parseFloat(interest) / 100) / 12), -12 * parseFloat(PayableYears)));
-        total = parseFloat(pmt) * Math.floor(parseFloat(PayableYears) * 12);
-        totalInterest = parseFloat(total) - parseFloat(principalAmount);
+        monthlyInterest = (parseFloat(total_Of_Units) * (parseFloat(interest) / 100)) / 12;
+        pmt = parseFloat(monthlyInterest) / (1 - Math.pow(1 + ((parseFloat(interest) / 100) / 12), -12 * parseFloat(daysOfBill)));
+        total = parseFloat(pmt) * Math.floor(parseFloat(daysOfBill) * 12);
+        totalInterest = parseFloat(total) - parseFloat(total_Of_Units);
         setTimeout(() => {
-            document.getElementById('principal').textContent = parseFloat(principalAmount).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('total_Of_Units').textContent = parseFloat(total_Of_Units).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
             document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
-            document.getElementById('loan-terms').textContent = parseFloat(PayableYears).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + (PayableYears > 1 ? " Yrs." : "")
-            document.getElementById('monthly-pay').textContent = parseFloat(pmt).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-pay').textContent = parseFloat(total).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-interest').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('bill_period_days').textContent = parseFloat(daysOfBill).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            document.getElementById('electriRateFinal').textContent = parseFloat(electriRatefinal).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            document.getElementById('total-pay').textContent = parseFloat(total).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            document.getElementById('total-interest').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
             end_loader()
         }, 500)
 
     })
-    loanForm.addEventListener('reset', function(e) {
+    loanForm.addEventListener('reset', function (e) {
         start_loader();
         setTimeout(() => {
-            document.getElementById('principal').textContent = ""
+            document.getElementById('total_Of_Units').textContent = ""
             document.getElementById('annual-interest').textContent = ""
-            document.getElementById('loan-terms').textContent = ""
-            document.getElementById('monthly-pay').textContent = ""
+            document.getElementById('bill_period_days').textContent = ""
+            document.getElementById('electriRateFinal').textContent = ""
             document.getElementById('total-pay').textContent = ""
             document.getElementById('total-interest').textContent = ""
             document.getElementById('result').style.display = 'none';
