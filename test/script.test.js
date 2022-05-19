650ad9d00b9d10f59954db837496d176f461de08
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-20 00:52:20 +0100

    Update script.test.js

--- test/script.test.js
+++ test/script.test.js
@@ -1,13 +1,3 @@
-'use strict';
-
-describe(
-    'script', () =>{
-        describe(
-            '#find', () =>{
-                it(
-                    'should return result when a file matches a term'
-                );
-            }
-        );
-    }
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a49b60a1680c3a39eb2ee977a0b79b369868a0814e2bbdcedf414e3218b10ff
+size 215
