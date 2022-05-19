6d01fc1b67a946567435162ab3bb094426302c59
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-19 13:56:40 +0100

    change the way of calculate bill
    the changes base on the CA requirements

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//two functions starting and endding
 window.start_loader = function () {
     const loader = document.getElementById('loader-holder')
     loader.style.display = 'flex';
@@ -10,32 +11,30 @@ window.end_loader = function () {
 window.onload = function () {
     setTimeout(() => {
         end_loader()
-    }, 500)
+    }, 500)//if we couldnt get response, set the paramater as 300
 
-    const loanForm = document.getElementById('calculate-loan-form')
+    const loanForm = document.getElementById('calculate-Bill-form')
     loanForm.addEventListener('submit', function (e) {
-        e.preventDefault()
-        start_loader();
-        const total_Of_Units = document.getElementById('number_Units').value;
-        const interest = document.getElementById('vat').value;
+        e.preventDefault()//if users don't type some on files which was requested on result, will remind users
+        start_loader();    //will call the method
+        const totalUnits = document.getElementById('number_Units').value;
+        const interest = document.getElementById('electriRate').value;
         const daysOfBill = document.getElementById('days_for_bill').value;
-        const electriRatefinal = document.getElementById('electriRate').value;
-        var monthly = 0,
-            pmt = 0,
-            total = 0,
-            totalInterest = 0,
-            monthlyInterest = 0;
-        monthlyInterest = (parseFloat(total_Of_Units) * (parseFloat(interest) / 100)) / 12;
-        pmt = parseFloat(monthlyInterest) / (1 - Math.pow(1 + ((parseFloat(interest) / 100) / 12), -12 * parseFloat(daysOfBill)));
-        total = parseFloat(pmt) * Math.floor(parseFloat(daysOfBill) * 12);
-        totalInterest = parseFloat(total) - parseFloat(total_Of_Units);
+        const vatRate = document.getElementById('vat').value;
+        
+        var total = 0,   //total without vat
+            totalWvat= 0;//total with vat
+
+        total = ((parseFloat(totalUnits)) * ((parseFloat(interest)))) + ((parseFloat(daysOfBill)) * 0.04);
+        totalWvat = (total * (parseFloat(vatRate) / 100)) + total;
+
         setTimeout(() => {
-            document.getElementById('total_Of_Units').textContent = parseFloat(total_Of_Units).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
+            document.getElementById('total_Of_Units').textContent = parseFloat(totalUnits).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('VAT').textContent = parseFloat(vatRate).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
             document.getElementById('bill_period_days').textContent = parseFloat(daysOfBill).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
-            document.getElementById('electriRateFinal').textContent = parseFloat(electriRatefinal).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            document.getElementById('TotalwithVat').textContent = parseFloat(totalWvat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
             document.getElementById('total-pay').textContent = parseFloat(total).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
-            document.getElementById('total-interest').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            //document.getElementById('vat cost').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
             end_loader()
@@ -46,11 +45,11 @@ window.onload = function () {
         start_loader();
         setTimeout(() => {
             document.getElementById('total_Of_Units').textContent = ""
-            document.getElementById('annual-interest').textContent = ""
+            document.getElementById('VAT').textContent = ""
             document.getElementById('bill_period_days').textContent = ""
-            document.getElementById('electriRateFinal').textContent = ""
+            document.getElementById('TotalwithVat').textContent = ""
             document.getElementById('total-pay').textContent = ""
-            document.getElementById('total-interest').textContent = ""
+
             document.getElementById('result').style.display = 'none';
             document.getElementById('reset-btn').style.display = 'none';
             end_loader()
