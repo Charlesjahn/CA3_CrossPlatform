853249c9df1896a30e09349caff3cca10ca704d1
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-19 23:52:59 +0100

    test file

---
+++ test/script.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe(
+    'script', () =>{
+        describe(
+            '#find', () =>{
+                it(
+                    'should return result when a file matches a term'
+                );
+            }
+        );
+    }
+);
