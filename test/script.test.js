8548fe92a5bd2c41eea2c38c8f4b1591f58eaa68
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-20 01:59:33 +0100

    add

--- test/script.test.js
+++ test/script.test.js
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fd3f48e1f9dee9a8e1b2bbf8ad7be60801ebb1d6770906f140fae42f2f7fcd24
-size 246
+'use strict';
+
+describe(
+    'script', () =>{
+        describe(
+            '#find', () =>{
+                it(
+                    'should return result when a file matches a term'
+                );
+            }
+        );
+    }
+);
